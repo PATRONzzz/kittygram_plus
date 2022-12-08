1397cee555a07275aa85f8292250b2d14718d2da
Androsov Artem <androsov.arte.v@gmail.com>
2022-12-08 18:01:48 +1100

    first

--- cats/models.py
+++ cats/models.py
@@ -1,11 +1,32 @@
 from django.db import models
+from django.urls import reverse
+
+
+class Owner(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+
+    # class Meta:
+    #     verbose_name = _("")
+    #     verbose_name_plural = _("s")
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    # def get_absolute_url(self):
+    #     return reverse("_detail", kwargs={"pk": self.pk})
 
 
 class Cat(models.Model):
     name = models.CharField(max_length=16)
     color = models.CharField(max_length=16)
     birth_year = models.IntegerField()
-
+    owner = models.ForeignKey(
+        Owner,
+        on_delete=models.CASCADE,
+        related_name='cats',
+        # verbose_name='Хозяин'
+    )
 
     def __str__(self):
         return self.name

750695dacc33f274ed81fb21b22c4ff268973536
Artem Androsov <androsov.artem.v@gmail.com>
2022-12-10 22:06:27 +1100

    10.12

--- cats/models.py
+++ cats/models.py
@@ -1,20 +1,19 @@
 from django.db import models
-from django.urls import reverse
+
+
+class Achievement(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
 
 
 class Owner(models.Model):
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
 
-    # class Meta:
-    #     verbose_name = _("")
-    #     verbose_name_plural = _("s")
-
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
-
-    # def get_absolute_url(self):
-    #     return reverse("_detail", kwargs={"pk": self.pk})
 
 
 class Cat(models.Model):
@@ -27,6 +26,18 @@ class Cat(models.Model):
         related_name='cats',
         # verbose_name='Хозяин'
     )
+    achievements = models.ManyToManyField(
+        Achievement,
+        through='AchievementCat',
+    )
 
     def __str__(self):
         return self.name
+
+
+class AchievementCat(models.Model):
+    achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE)
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.achievement} {self.cat}'
--- cats/serializers.py
+++ cats/serializers.py
@@ -1,11 +1,26 @@
 from rest_framework import serializers
 
-from .models import Cat, Owner
+from .models import Achievement, AchievementCat, Cat, Owner
+
+
+class AchievementSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Achievement
+        fields = (
+            'id',
+            'name',
+        )
 
 
 class CatSerializer(serializers.ModelSerializer):
-    owner = serializers.StringRelatedField(
-        read_only=True,
+    # owner = serializers.StringRelatedField(
+    #     read_only=True,
+    # )
+    achievements = AchievementSerializer(
+        # read_only=True,
+        many=True,
+        required=False,
     )
 
     class Meta:
@@ -15,8 +30,34 @@ class CatSerializer(serializers.ModelSerializer):
             'name',
             'color',
             'birth_year',
-            'owner'
+            'owner',
+            'achievements',
         )
+
+    def create(self, validated_data: dict):
+        # Уберем спосок достижений из словаря validated_data и сохраним его
+        if 'achievements' not in self.initial_data:
+            cat = Cat.objects.create(**validated_data)
+            return cat
+
+        achievements = validated_data.pop('achievements')
+
+        # создание нового котика
+        cat = Cat.objects.create(**validated_data)
+
+        # для каждого достижения
+        for achievment in achievements:
+            # создание новой записи или полчение существующего экземляра из базы данных
+            current_achievment, status = Achievement.objects.get_or_create(
+                **achievment)
+            # Ссылка на каждое достижение во всппомогательную таблицу с указанием
+            # к каком котику она относится
+            AchievementCat.objects.create(
+                achievement=current_achievment,
+                cat=cat
+            )
+
+        return cat
 
 
 class OwnerSerializer(serializers.ModelSerializer):
@@ -30,5 +71,5 @@ class OwnerSerializer(serializers.ModelSerializer):
         fields = (
             'first_name',
             'last_name',
-            'cats'
+            'cats',
         )
